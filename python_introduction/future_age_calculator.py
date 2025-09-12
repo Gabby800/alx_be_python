fffbe8026bcd7653196be17ae9359a68f3bf4845
Gabriel <yanksongabriel4@gmai.com>
2025-09-12 12:58:35 +0000

    f-string in future_age-calculator fixed

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,8 +1,11 @@
-#Prompt user for their current age
-current_age = int(input("How  old are you?"))
+# future_age_calculator.py
 
-#Calculate the future age
-future_age = current_age + 27
+# Prompt the user for their current age
+current_age = int(input ("How old are you?) "))
 
-#Print the results
+# Calculate future age in 2050
+years_to_add = 2050 - 2023   # 27 years
+future_age = current_age + years_to_add
+
+# Print the result
 print(f"In 2050, you will be {future_age} years old.")
